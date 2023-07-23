97a2078e5baae5b48e7bd04a4cc4508ab7927571
Guitho60 <cmdcntr.dm@gmail.com>
2023-07-23 22:46:09 +0200

    US03 : Ajout d'une boucle pour réaliser une autre opération

--- Calculatrice.py
+++ Calculatrice.py
@@ -1,34 +1,39 @@
 #Definition des constantes&variables
 contact = "contact-calculatricePython@gmail.com"
-msgerror = False
+icontinue = True
 
-#Saisie de l'opération par l'utilisateur
-chaine = input("Veuillez saisir une opération de la forme a + b : ")
-print(chaine)
+while icontinue:
+    msgerror = False
 
-chaine = chaine.split(" ")
-print(chaine)
+    #Saisie de l'opération par l'utilisateur
+    chaine = input("Veuillez saisir une opération de la forme a + b : ")
+    print(chaine)
 
-nombre1 = chaine[0]
-operation = chaine[1]
-nombre2 = chaine[2]
+    chaine = chaine.split(" ")
+    print(chaine)
 
-if operation == "+":
-    resultat = float(nombre1) + float(nombre2)
-elif operation == "-":
-    resultat = float(nombre1) - float(nombre2)
-elif operation == "*":
-    resultat = float(nombre1) * float(nombre2)
-elif operation == "/":
-    resultat = float(nombre1) / float(nombre2)
-elif operation == "modulo":
-    resultat = float(nombre1) % float(nombre2)
-else:
-    msgerror = True
+    nombre1 = chaine[0]
+    operation = chaine[1]
+    nombre2 = chaine[2]
 
-if not msgerror :
-    print(f"Résultat = {resultat}")
-else:
-    print("Erreur la chaine saisie ne correspond pas à une opération.")
+    if operation == "+":
+        resultat = float(nombre1) + float(nombre2)
+    elif operation == "-":
+        resultat = float(nombre1) - float(nombre2)
+    elif operation == "*":
+        resultat = float(nombre1) * float(nombre2)
+    elif operation == "/":
+        resultat = float(nombre1) / float(nombre2)
+    elif operation == "modulo":
+        resultat = float(nombre1) % float(nombre2)
+    else:
+        msgerror = True
 
-print(f"Merci d'avoir utilisé notre calculatrice !! Adresse de contact : {contact}")
+    if not msgerror :
+        print(f"Résultat = {resultat}")
+    else:
+        print("Erreur la chaine saisie ne correspond pas à une opération.")
+
+    print(f"Merci d'avoir utilisé notre calculatrice !! Adresse de contact : {contact}")
+    #icontinue = input("Souhaitez-vous réaliser une autre opération ? 1 = Oui sinon Non : ")
+    icontinue = True if input("Souhaitez-vous réaliser une autre opération ? (1 = Oui) : ") == "1" else False
