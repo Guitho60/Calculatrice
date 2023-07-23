df06a4b66cd3b6c596049f70e0c13efbc880615c
Guitho60 <cmdcntr.dm@gmail.com>
2023-07-23 21:58:42 +0200

    Ajout de la logique, calcul et affichage du résultat

--- Calculatrice.py
+++ Calculatrice.py
@@ -1,5 +1,28 @@
 #Saisie de l'opération par l'utilisateur
+msgerror = False
+
 chaine = input("Veuillez saisir une opération de la forme a + b : ")
 print(chaine)
 
-chaine
+chaine = chaine.split(" ")
+print(chaine)
+
+nombre1 = chaine[0]
+operation = chaine[1]
+nombre2 = chaine[2]
+
+if operation == "+":
+    resultat = float(nombre1) + float(nombre2)
+elif operation == "-":
+    resultat = float(nombre1) - float(nombre2)
+elif operation == "*":
+    resultat = float(nombre1) * float(nombre2)
+elif operation == "/":
+    resultat = float(nombre1) / float(nombre2)
+else:
+    msgerror = True
+
+if not msgerror :
+    print(f"Résultat = {resultat}")
+else:
+    print("Erreur la chaine saisie ne correspond pas à une opération.")
